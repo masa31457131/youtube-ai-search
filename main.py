cd2a620ef3d6ff2ba4e63e814c70d94eb2d22224
masa31457131 <73268315+masa31457131@users.noreply.github.com>
2025-04-13 22:05:00 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("paraphrase-MiniLM-L3-v2")
 video_data = []  # [(title, description, url, thumbnail), ...]
 index = None     # FAISS index
 
